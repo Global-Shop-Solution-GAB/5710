d6f66c7657419af310b73deeab7d585024dcd985
Service Web <sw@gssmail.com>
2024-03-14 15:05:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5710\GCG_5710_KitSheet.sql

---
+++ GCG_5710_KitSheet.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_5710_KitSheet"(
+ "Kit_No" VARCHAR(25) NOT NULL ,
+ "Item_No" VARCHAR(40),
+ "Machine" VARCHAR(15),
+ "Tool_No" VARCHAR(40),
+ "Tool_Desc" VARCHAR(60),
+ "Qty" VARCHAR(10),
+ "Location" VARCHAR(12),
+ "Tool_Comment" VARCHAR(40));
