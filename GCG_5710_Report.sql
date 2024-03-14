5f8019a179ea21d8a50d9d167c39ae6a306540b4
Service Web <sw@gssmail.com>
2024-03-14 15:05:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5710\GCG_5710_Report.sql

---
+++ GCG_5710_Report.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GCG_5710_Report"(
+ "Kit_No" VARCHAR(25),
+ "Item_No" VARCHAR(40),
+ "Machine" VARCHAR(15),
+ "Tool_No" VARCHAR(40),
+ "Tool_Desc" VARCHAR(60),
+ "Qty" DECIMAL(8,2),
+ "Location" VARCHAR(12),
+ "Tool_Comment" VARCHAR(40),
+ "Router_SEQ" VARCHAR(6),
+ "WO" VARCHAR(10),
+ "Terminal" CHAR(3));
